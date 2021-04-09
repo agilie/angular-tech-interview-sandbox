7d8451b7d09cf8f5fc206f4aa4ebbe6575aee33f
Sergey Mell <sergey.mell@agilie.com>
2021-04-09 11:59:32 +0300

    Added validation errors Improved custom input Added sign in component logic

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../_services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -8,9 +10,31 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = new FormGroup({});
+
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submitForm(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.authService.signIn(this.form.value.email, this.form.value.password)
+      .subscribe(res => {
+        alert('Form submitted successfully!!!');
+      });
   }
 
 }

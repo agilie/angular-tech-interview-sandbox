f36e0bfe0734d1c47486c43432c82fafd8fdd070
Sergey Mell <sergey.mell@agilie.com>
2021-04-09 11:19:31 +0300

    Added dashboard

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {HomeRoutingModule} from './home-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HomeRoutingModule,
+  ],
+  declarations: [
+    DashboardComponent
+  ],
 })
 export class HomeModule { }

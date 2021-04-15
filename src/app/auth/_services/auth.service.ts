ab32ad7b0f4653d02aec449969d81e12130946fe
Sergey Mell <sergey.mell@agilie.com>
2021-04-15 10:58:47 +0300

    Added fake requests for user sign-up and sign-in

--- src/app/auth/_services/auth.service.ts
+++ src/app/auth/_services/auth.service.ts
@@ -1,25 +1,29 @@
 import {Injectable} from '@angular/core';
-import {Observable, timer} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {environment as env} from '../../../environments/environment';
 
 @Injectable()
 export class AuthService {
+
+  constructor(private http: HttpClient) {
+  }
 
   /**
    * Creates user in the application
    */
   signUp(email: string, password: string): Observable<object> {
-    return timer(1000).pipe(
-      map(() => ({}))
-    );
+    const body = {
+      email,
+      password
+    };
+    return this.http.post(`${env.apiHost}/users`, body);
   }
 
   /**
    * Authorizes existing user in the application
    */
   signIn(email: string, password: string): Observable<object> {
-    return timer(1000).pipe(
-      map(() => ({}))
-    );
+    return this.http.get(`${env.apiHost}/users/1`);
   }
 }
